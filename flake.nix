9e3324e5f011a338c45622fda1402e2b14e51a97
Doridian <git@doridian.net>
2025-10-03 11:55:51 -0700

    Named package

--- flake.nix
+++ flake.nix
@@ -7,16 +7,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        package = pkgs.buildGoModule {
+          pname = "uds-proxy";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = "sha256-TB+vWr8ortGSZeggf/hnHpfaJxmI29W6swkGywZLeWs=";
+          buildInputs = [];
+        };
       in
       {
         packages = {
-          default = pkgs.buildGoModule {
-            pname = "uds-proxy";
-            version = "0.1.0";
-            src = ./.;
-            vendorHash = "sha256-TB+vWr8ortGSZeggf/hnHpfaJxmI29W6swkGywZLeWs=";
-            buildInputs = [];
-          };
+          default = package;
+          uds-proxy = package;
         };
       });
 }
